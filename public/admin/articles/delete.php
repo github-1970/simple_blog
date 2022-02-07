24b93747f77534f14ea2aca083af88bcb80f7685
github-1970 <github1970@gmail.com>
2022-02-07 18:59:17 +0330

    when deleting a post, delete post image

--- public/admin/articles/delete.php
+++ public/admin/articles/delete.php
@@ -7,6 +7,14 @@ try{
     $delete = sanitizeString($_GET['delete']);
 
     if($id && $delete){
+      // for delete post image
+      $post_query = 'SELECT image FROM posts WHERE id = :id';
+      $stmt = $conn->prepare($post_query);
+      $stmt->execute(['id'=>$id]);
+      $post_image = $stmt->fetch(PDO::FETCH_OBJ)->image;
+      $image_path = PUBLIC_DIR . '/img/posts/' .$post_image;
+      file_exists($image_path) ? unlink($image_path) : '';
+
       $delete_query = 'DELETE FROM posts WHERE id = :id';
       $stmt = $conn->prepare($delete_query);
       $stmt->execute(['id'=>$id]);
